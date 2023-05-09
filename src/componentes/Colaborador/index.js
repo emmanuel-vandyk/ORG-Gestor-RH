9856a1077eb8bac5b896430320ebf97f94b669b2
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2023-05-09 01:08:42 -0300

    Crear componente Colaborador y dar estilos

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import "./Colaborador.css"
+
+const Colaborador = () => {
+    return <div className="colaborador">
+        <div className="encabezado">
+            <img src="https://github.com/emmanuel-vandyk.png" alt="Emmanuel" />
+        </div>
+        <div className="info">
+            <h4>Emmanuel van Dyk</h4>
+            <h5>Programador</h5>
+        </div>
+    </div>
+}
+
+export default Colaborador;
